e9791ba122c51e551ba63d339822523ab30b14c7
Anomalous Alpha <blackcarrera@msn.com>
2021-07-11 18:33:50 -0600

    fixed the tought controller, syntax error somewhere so started over , now seems good

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,41 +2,47 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
     getAllThoughts(req, res) {
-        .sort ({ _id: -1 })
+        Thought.find({})
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(er);
+            res.sendStatus(400);
+        });
+    },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
-            res.sendStatus(400);
-        })
-    },
-
-    getThoughtByid({ params }< res) {
-        Thought.findOne({ _id: params.id })
-        .then(dbThoughtData => res.json(dbUserData))
-        .catch(err => {
-            consol.log(err);
             res.sendStatus(400);
         });
     },
 
     createThought({ body }, res) {
-        Thought.create(body)
+        Thought.create( body)
         .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => res.json(err))
+        .catch(err => res.json(er));
     },
 
     updateThought({ params, body }, res) {
-        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        Thought.findOneAndUpdate({ _id: params.id }, body, {new: true, runValidators: true })
         .then(dbThoughtData => {
             if (!dbThoughtData) {
-                res.status(404).json({ message: 'No thoughts found by that id!' });
+                res.status(404).json({ message: 'No thought found by that id~' });
+                return;
             }
-        }) 
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
     },
 
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.id })
-        .then(dbThoughtData => res.json(dbuserData))
+        .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => res.json(err));
     }
 }
+
+module.exports = userController;
